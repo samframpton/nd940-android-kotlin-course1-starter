8bff2ff7c111d2b402ae9e82a90b47db50069ef0
Sam Frampton <12323716+samframpton@users.noreply.github.com>
2020-12-09 16:27:37 +0000

    Enable data binding

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,12 +2,16 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
