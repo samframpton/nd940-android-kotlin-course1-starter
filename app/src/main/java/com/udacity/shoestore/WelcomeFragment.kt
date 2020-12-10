706f072df6a39e133c089fa13771bb619af701ff
Sam Frampton <12323716+samframpton@users.noreply.github.com>
2020-12-10 14:36:04 +0000

    Add instruction destination

--- app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -16,7 +17,9 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_welcome, container, false)
+            inflater, R.layout.fragment_welcome, container, false)
+        binding.buttonWelcomeNext.setOnClickListener(Navigation.createNavigateOnClickListener(
+            WelcomeFragmentDirections.actionWelcomeFragmentToInstructionFragment()))
         return binding.root
     }
 
