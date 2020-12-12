517af7b2310ce9f834890a22604ab728446886e6
Sam Frampton <12323716+samframpton@users.noreply.github.com>
2020-12-12 11:39:49 +0000

    Display all shoes in ShoeListFragment

--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -22,21 +22,24 @@ class ShoeListFragment : Fragment() {
         val binding: FragmentShoeListBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe_list, container, false)
         val viewModel: ShoeViewModel by activityViewModels()
-        viewModel.shoeList.observe(viewLifecycleOwner,
-            Observer { if (it.isNotEmpty()) addShoe(it.last(), binding.linearLayoutShoes) })
+        viewModel.shoeList.observe(
+            viewLifecycleOwner, Observer { addShoes(it, binding.linearLayoutShoes) })
         binding.fabAddNewShoe.setOnClickListener(Navigation.createNavigateOnClickListener(
             ShoeListFragmentDirections.actionShoeListFragmentToShoeDetailFragment()))
         setHasOptionsMenu(true)
         return binding.root
     }
 
-    private fun addShoe(shoe: Shoe, shoeListView: ViewGroup) {
-        val binding: ItemShoeBinding = DataBindingUtil.inflate(
-            layoutInflater, R.layout.item_shoe, shoeListView, true)
-        binding.textViewShoeName.text = shoe.name
-        binding.textViewShoeCompany.text = getString(R.string.shoe_company_format, shoe.company)
-        binding.textViewShoeSize.text = getString(R.string.shoe_size_format, shoe.size)
-        binding.textViewShoeDescription.text = getString(R.string.shoe_description_format, shoe.description)
+    private fun addShoes(shoes: ArrayList<Shoe>, shoeListView: ViewGroup) {
+        for (shoe in shoes) {
+            val binding: ItemShoeBinding = DataBindingUtil.inflate(
+                layoutInflater, R.layout.item_shoe, shoeListView, true)
+            binding.textViewShoeName.text = shoe.name
+            binding.textViewShoeCompany.text = getString(R.string.shoe_company_format, shoe.company)
+            binding.textViewShoeSize.text = getString(R.string.shoe_size_format, shoe.size)
+            binding.textViewShoeDescription.text =
+                getString(R.string.shoe_description_format, shoe.description)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
